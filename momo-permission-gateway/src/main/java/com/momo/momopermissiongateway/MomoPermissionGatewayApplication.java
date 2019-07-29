ae98041c321a08b8a15208fd09899c60064b34cd
李杰 <15050451502@163.com>
2019-07-29 23:39:59 +0800

    框架搭建

---
+++ momo-permission-gateway/src/main/java/com/momo/momopermissiongateway/MomoPermissionGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.momo.momopermissiongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient //让注册中心能够发现 ,可以是其他注册中心
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
+public class MomoPermissionGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoPermissionGatewayApplication.class, args);
+    }
+
+}
---
+++ momo-permission-gateway/src/test/java/com/momo/momopermissiongateway/MomoPermissionGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.momo.momopermissiongateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MomoPermissionGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
