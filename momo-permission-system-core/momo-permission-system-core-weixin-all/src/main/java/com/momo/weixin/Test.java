0c39a0e967ce85e8a9e082525b0c8b2f9bcc1a3d
Jie Li <15050451502@163.com>
2019-09-07 10:02:53 +0800

    加入微信模块

---
+++ momo-permission-system-core/momo-permission-system-core-weixin-all/src/main/java/com/momo/weixin/Test.java
@@ -0,0 +1,14 @@
+package com.momo.weixin;
+
+/**
+ * @ProjectName: momo-cloud-permission
+ * @Package: com.momo.weixin
+ * @Description: TODO
+ * @Author: Jie Li
+ * @CreateDate: 2019/9/7 0007 10:02
+ * @UpdateDate: 2019/9/7 0007 10:02
+ * @Version: 1.0
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+public class Test {
+}
