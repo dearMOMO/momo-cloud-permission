151d6260c4bd972f207247f10dd3a7a18cbe39ea
Jie Li <15050451502@163.com>
2019-09-19 15:17:54 +0800

    微信相关配置

--- momo-permission-system-core/momo-permission-system-core-common/src/main/java/com/momo/common/common/JSONResult.java
+++ momo-permission-system-core/momo-permission-system-core-common/src/main/java/com/momo/common/common/JSONResult.java
@@ -1,19 +1,17 @@
 package com.momo.common.common;
 
 
-
 /**
- * 
  * @Description: 自定义响应数据结构
- * 				这个类是提供给门户，ios，安卓，微信商城用的
- * 				门户接受此类数据后需要使用本类的方法转换成对于的数据类型格式（类，或者list）
- * 				其他自行处理
- * 				200：表示成功
- * 				500：表示错误，错误信息在msg字段中
- * 				501：bean验证错误，不管多少个错误都以map形式返回
- * 				502：拦截器拦截到用户token出错
- * 				555：异常抛出信息
- * 			    ***:其他自定义异常
+ * 这个类是提供给门户，ios，安卓，微信商城用的
+ * 门户接受此类数据后需要使用本类的方法转换成对于的数据类型格式（类，或者list）
+ * 其他自行处理
+ * 200：表示成功
+ * 500：表示错误，错误信息在msg字段中
+ * 501：bean验证错误，不管多少个错误都以map形式返回
+ * 502：拦截器拦截到用户token出错
+ * 555：异常抛出信息
+ * ***:其他自定义异常
  */
 public class JSONResult {
 
@@ -26,41 +24,52 @@ public class JSONResult {
 
     // 响应中的数据
     private Object data;
-    
-    private String ok;	// 不使用
+
+    private String ok;    // 不使用
 
     public static JSONResult build(Integer status, String msg, Object data) {
         return new JSONResult(status, msg, data);
     }
+
     public static JSONResult ok(Object data) {
         return new JSONResult(data);
     }
+
     public static JSONResult ok(String msg, Object data) {
-        return new JSONResult(msg,data);
+        return new JSONResult(msg, data);
     }
 
     public static JSONResult ok() {
-        return new JSONResult(null,null);
+        return new JSONResult(null, null);
     }
-    
+
     public static JSONResult errorMsg(String msg) {
         return new JSONResult(500, msg, null);
     }
-    
+
+    public static JSONResult errorMsg(Integer status, String msg) {
+        if (status == null) {
+            return new JSONResult(500, msg, null);
+        }
+        return new JSONResult(status, msg, null);
+    }
+
     public static JSONResult errorMap(Object data) {
         return new JSONResult(501, "error", data);
     }
-    
+
     public static JSONResult errorTokenMsg(String msg) {
         return new JSONResult(502, msg, null);
     }
-    
+
     public static JSONResult errorException(String msg) {
         return new JSONResult(555, msg, null);
     }
+
     public static JSONResult errorException(Integer status, String msg, Object data) {
         return new JSONResult(status, msg, data);
     }
+
     public JSONResult() {
 
     }
@@ -74,11 +83,13 @@ public class JSONResult {
         this.msg = msg;
         this.data = data;
     }
+
     public JSONResult(Object data) {
         this.status = 200;
         this.msg = "OK";
         this.data = data;
     }
+
     public JSONResult(String msg, Object data) {
         this.status = 200;
         this.msg = msg;
@@ -113,12 +124,12 @@ public class JSONResult {
         this.data = data;
     }
 
-	public String getOk() {
-		return ok;
-	}
+    public String getOk() {
+        return ok;
+    }
 
-	public void setOk(String ok) {
-		this.ok = ok;
-	}
+    public void setOk(String ok) {
+        this.ok = ok;
+    }
 
 }
