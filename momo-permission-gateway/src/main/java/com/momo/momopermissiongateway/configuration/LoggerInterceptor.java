c8d88e55ce1a614ac6d7643aa676e6a9a177594c
Jie Li <15050451502@163.com>
2019-11-06 17:12:46 +0800

    更改cloud版本

---
+++ momo-permission-gateway/src/main/java/com/momo/momopermissiongateway/configuration/LoggerInterceptor.java
@@ -0,0 +1,116 @@
+//package com.momo.momopermissiongateway.configuration;
+//
+//import io.micrometer.core.lang.NonNull;
+//import lombok.extern.slf4j.Slf4j;
+//import okhttp3.*;
+//import okhttp3.internal.http.HttpHeaders;
+//import okio.Buffer;
+//import okio.BufferedSource;
+//
+//import java.io.BufferedReader;
+//import java.io.EOFException;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.nio.charset.Charset;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.charset.UnsupportedCharsetException;
+//
+///**
+// * @ClassName: LoggerInterceptor
+// * @Author: Jie Li
+// * @Date 2019-10-25 16:50
+// * @Description: TODO
+// * @Version: 1.0
+// * <p>Copyright: Copyright (c) 2019</p>
+// **/
+//@Slf4j
+//public class LoggerInterceptor implements Interceptor {
+//    private static final Charset UTF8 = StandardCharsets.UTF_8;
+//
+//    @Override
+//    public Response intercept(@NonNull Chain chain) throws IOException {
+//        Request request = chain.request();
+//        Response response = chain.proceed(request);
+//
+//        ResponseBody responseBody = response.body();
+//        long contentLength = responseBody.contentLength();
+//
+//        if (!bodyEncoded(response.headers())) {
+//            BufferedSource source = responseBody.source();
+//            source.request(Long.MAX_VALUE); // Buffer the entire body.
+//            Buffer buffer = source.buffer();
+//
+//            Charset charset = UTF8;
+//            MediaType contentType = responseBody.contentType();
+//            if (contentType != null) {
+//                try {
+//                    charset = contentType.charset(UTF8);
+//                } catch (UnsupportedCharsetException e) {
+//                    return response;
+//                }
+//            }
+//
+//            if (!isPlaintext(buffer)) {
+//                return response;
+//            }
+//
+//            if (contentLength != 0) {
+//                String result = buffer.clone().readString(charset);
+//                log.info(" response.url():" + response.request().url());
+//                log.info(" response.body():" + result);
+//                //得到所需的string，开始判断是否异常
+//                //***********************do something*****************************
+//
+//            }
+//
+//        }
+//        return response;
+//
+//
+////        // 拦截请求，获取到该次请求的request
+////        Request request = chain.request();
+////        // 执行本次网络请求操作，返回response信息
+////        Response response = chain.proceed(request);
+////        for (String key : request.headers().toMultimap().keySet()) {
+////            log.info("zp_test header: {" + key + " : " + request.headers().toMultimap().get(key) + "}");
+////        }
+////        log.info("zp_test url: {}" , request.url().uri().toString());
+////        ResponseBody responseBody = response.body();
+////        if (HttpHeaders.hasBody(response) && responseBody != null) {
+////            BufferedReader bufferedReader = new BufferedReader(new
+////                    InputStreamReader(responseBody.byteStream(), StandardCharsets.UTF_8));
+////            String result;
+////            while ((result = bufferedReader.readLine()) != null) {
+////                log.info("zp_testresponse: {}" , result);
+////            }
+////        }
+////        // 注意，这样写，等于重新创建Request，获取新的Response，避免在执行以上代码时，
+////        // 调用了responseBody.string()而不能在返回体中再次调用。
+////        return response.newBuilder().body(responseBody).build();
+//    }
+//
+//    private boolean bodyEncoded(Headers headers) {
+//        String contentEncoding = headers.get("Content-Encoding");
+//        return contentEncoding != null && !contentEncoding.equalsIgnoreCase("identity");
+//    }
+//
+//    static boolean isPlaintext(Buffer buffer) throws EOFException {
+//        try {
+//            Buffer prefix = new Buffer();
+//            long byteCount = buffer.size() < 64 ? buffer.size() : 64;
+//            buffer.copyTo(prefix, 0, byteCount);
+//            for (int i = 0; i < 16; i++) {
+//                if (prefix.exhausted()) {
+//                    break;
+//                }
+//                int codePoint = prefix.readUtf8CodePoint();
+//                if (Character.isISOControl(codePoint) && !Character.isWhitespace(codePoint)) {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        } catch (EOFException e) {
+//            return false; // Truncated UTF-8 sequence.
+//        }
+//    }
+//}
---
+++ momo-permission-gateway/src/main/java/com/momo/momopermissiongateway/configuration/OkHttpConfig.java
@@ -0,0 +1,55 @@
+//package com.momo.momopermissiongateway.configuration;
+//
+///**
+// * @ClassName: OkHttpConfig
+// * @Author: Jie Li
+// * @Date 2019-10-25 16:38
+// * @Description: TODO
+// * @Version: 1.0
+// * <p>Copyright: Copyright (c) 2019</p>
+// **/
+//
+//import feign.Feign;
+//import feign.codec.Encoder;
+//import feign.form.FormEncoder;
+//import okhttp3.*;
+//import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+//import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.io.IOException;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * 配置 okhttp 与连接池
+// * ConnectionPool 默认创建5个线程，保持5分钟长连接
+// */
+////@Configuration
+////@ConditionalOnClass(Feign.class)
+////@AutoConfigureBefore(FeignAutoConfiguration.class) //SpringBoot自动配置
+//public class OkHttpConfig {
+//
+//    // 默认老外留给你彩蛋中文乱码，加上它就 OK
+//   /* @Bean
+//    public Encoder encoder() {
+//        return new FormEncoder();
+//    }*/
+//
+//    @Bean
+//    public okhttp3.OkHttpClient okHttpClient() {
+//        return new okhttp3.OkHttpClient.Builder()
+//                //设置连接超时
+//                .connectTimeout(10, TimeUnit.SECONDS)
+//                //设置读超时
+//                .readTimeout(10, TimeUnit.SECONDS)
+//                //设置写超时
+//                .writeTimeout(10, TimeUnit.SECONDS)
+//                //是否自动重连
+//                .retryOnConnectionFailure(true)
+//                .addInterceptor(new LoggerInterceptor())
+//                .connectionPool(new ConnectionPool(10, 5L, TimeUnit.MINUTES))
+//                .build();
+//    }
+//}
