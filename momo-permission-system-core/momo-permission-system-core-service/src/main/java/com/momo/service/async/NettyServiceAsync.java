59581774629f5b03c858a16b45e6b393e2a4281d
Jie Li <15050451502@163.com>
2020-04-10 23:24:54 +0800

    实时在线人数统计 完善注释

--- momo-permission-system-core/momo-permission-system-core-service/src/main/java/com/momo/service/async/NettyServiceAsync.java
+++ momo-permission-system-core/momo-permission-system-core-service/src/main/java/com/momo/service/async/NettyServiceAsync.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Future;
  * @ClassName: NettyServiceAsync
  * @Author: Jie Li
  * @Date 2019-11-25 17:07
- * @Description: TODO
+ * @Description: Netty相关
  * @Version: 1.0
  * <p>Copyright: Copyright (c) 2019</p>
  **/
@@ -23,6 +23,12 @@ public class NettyServiceAsync {
     @Autowired
     private NettyHandlerService nettyHandlerService;
 
+    /**
+     * 实时在线人数统计
+     *
+     * @param symbol
+     * @return
+     */
     public Future<String> onlineCount(String symbol) {
         return nettyHandlerService.onlineCount(symbol);
     }
